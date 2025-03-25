a14a3101572bc55afbfd4bf3a50ca4b77e348b30
LBerres <Leandro.motorhead@gmail.com>
2025-03-25 19:27:59 -0300

    Implementado a Versão 1: Estrutura básica e entrada do usuário

--- JogoDaForca.ConsoleApp/Program.cs
+++ JogoDaForca.ConsoleApp/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace JogoDaForca.ConsoleApp
+﻿using System;
+
+namespace JogoDaForca.ConsoleApp
 {
     internal class Program
     {
+        // Versão 1: Estrutura básica e entrada do usuário
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("---------------------------------------------");
+                Console.WriteLine("Jogo da Forca");
+                Console.WriteLine("---------------------------------------------");
+
+                Console.Write("Digite Uma Letra");
+                char chute = Console.ReadLine()[0]; // Obtém Apenas Um Caractere Que o Usuário Digita
+
+                Console.WriteLine(chute);
+
+                Console.ReadLine();
+            }
         }
+
     }
 }
