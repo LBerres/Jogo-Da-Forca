4f660e073bff8327d6c12c174f9559e9e0a27b71
LBerres <Leandro.motorhead@gmail.com>
2025-03-25 21:02:03 -0300

    Implementado a Versão 3: Verificação do input

--- JogoDaForca.ConsoleApp/Program.cs
+++ JogoDaForca.ConsoleApp/Program.cs
@@ -4,7 +4,7 @@ namespace JogoDaForca.ConsoleApp
 {
     internal class Program
     {
-        // Versão 2: Exibir palavra oculta com traços
+        // Versão 3: Verificação do input
         static void Main(string[] args)
         {
             while (true)
@@ -20,21 +20,69 @@ namespace JogoDaForca.ConsoleApp
                     letrasEncontradas[caractere] = '_';
                 }
 
-                string dicaDaPalavra = String.Join(" ", letrasEncontradas);
+                int quantidadeDeErros = 0;
+                bool jogadorEnforcou = false;
+                bool jogadorAcertou = false;
 
-                Console.Clear();
-                Console.WriteLine("---------------------------------------------");
-                Console.WriteLine("Jogo da Forca");
-                Console.WriteLine("---------------------------------------------");
-                Console.WriteLine("Palavra Secreta: " + dicaDaPalavra);
-                Console.WriteLine("---------------------------------------------");
+                do
+                {
+                    string dicaDaPalavra = String.Join(" ", letrasEncontradas);
 
-                Console.Write("Digite Uma Letra: ");
-                char chute = Console.ReadLine()[0]; // Obtém Apenas Um Caractere Que o Usuário Digita
+                    Console.Clear();
+                    Console.WriteLine("---------------------------------------------");
+                    Console.WriteLine("Jogo da Forca");
+                    Console.WriteLine("---------------------------------------------");
+                    Console.WriteLine("Palavra Secreta: " + dicaDaPalavra);
+                    Console.WriteLine("---------------------------------------------");
+                    Console.WriteLine("Quantidade de Erros: " + quantidadeDeErros);
+                    Console.WriteLine("---------------------------------------------");
 
-                Console.WriteLine(chute);
+                    Console.Write("Digite Uma Letra: ");
+                    char chute = Console.ReadLine()[0]; // char = Obtém Apenas Um Caractere Que o Usuário Digita
+
+                    bool letraFoiEncontrada = false;
+
+                    for (int contador = 0; contador < palavraSecreta.Length; contador++)
+                    {
+                        char letraAtual = palavraSecreta[contador];
+
+                        if (chute == letraAtual)
+                        {
+                            letrasEncontradas[contador] = letraAtual;
+                            letraFoiEncontrada = true;
+                        }
+                        
+                    }
+
+                    if (letraFoiEncontrada == false)
+                        quantidadeDeErros++;
+
+                    dicaDaPalavra = String.Join("", letrasEncontradas);
+
+                    jogadorAcertou = dicaDaPalavra == palavraSecreta;
+                    // Jogador Poderá Cometer 5 Erros antes de Perder
+                    jogadorEnforcou = quantidadeDeErros > 5;
+
+                    if (jogadorAcertou)
+                    {
+                        Console.WriteLine("---------------------------------------------");
+                        Console.WriteLine("Você Acertou a Palavra Secreta! Era: " + palavraSecreta);
+                        Console.WriteLine("---------------------------------------------");
+
+                    }  
+                    else if (jogadorEnforcou)
+                    {
+                        Console.WriteLine("---------------------------------------------");
+                        Console.WriteLine("Que Azar, Tente Novamente! A Palavra Era: " + palavraSecreta);
+                        Console.WriteLine("---------------------------------------------");
+
+                    }
+
+                } while (jogadorAcertou == false && jogadorEnforcou == false); // || = OU
 
                 Console.ReadLine();
+
+                
             }
         }
 
